29ed8d33078118017084b6802618a29865a9585f
Anam Shaikh <anam.shk@outlook.com>
2021-11-10 19:38:39 -0500

    commit changes with login and logout

--- express_server.js
+++ express_server.js
@@ -1,16 +1,21 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
+
 const app = express();
 const PORT = 8080; // default port 8080
 
 //Buffer - comes before all routes
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 
 app.set("view engine", "ejs");
+
 
 const generateRandomString = () => {
   return Math.random().toString(36).substr(2, 6);
 };
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -26,22 +31,31 @@ app.post("/urls", (req, res) => {
 });
 
 //urls
-app.get('/u/:shortURL', (req, res) => {
+app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = {
+    urls: urlDatabase,
+    username: req.cookies["username"]};
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    username: req.cookies["username"]
+  };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  const templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies["username"]
+  };
   console.log(templateVars);
   if (!templateVars.longURL) {
     res.redirect('/urls');
@@ -76,6 +90,20 @@ app.post("/urls/:id", (req,res) =>{
   urlDatabase[shortURL] = updatedLongURL;
   res.redirect("/urls/");
 });
+
+//POST to login, redirect to /urls apge and display username successfully logged in
+app.post("/login", (req,res) =>{
+  const { username } = req.body;
+  res.cookie("username", username);
+  res.redirect("/urls");
+});
+
+//POST when user logs out
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect("/urls");
+});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
